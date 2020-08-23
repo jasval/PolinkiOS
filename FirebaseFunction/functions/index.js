0716a060fc80dc51d370af1a4b8d526ccef7f818
Josh Valdivia <saldana.val@gmail.com>
2020-08-23 02:05:08 -0400

    Revert "starting implementation of firebase cloud functions for notifications to end users"
    This reverts commit 401951705a706a0c02ef79ded8a1fb5658081aa2.

--- FirebaseFunction/functions/index.js
+++ FirebaseFunction/functions/index.js
@@ -1,11 +1,9 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const firebase_tools = require('firebase-tools');
-const { firestore } = require('firebase-admin');
 
 admin.initializeApp();
 
-const db = admin.firestore();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -47,90 +45,3 @@ exports.recursiveDelete = functions
       path: path 
     };
   });
-
-exports.sendNotification = functions.firestore
-  .document('rooms/{roomId}')
-// database.ref('/rooms/{roomId}')
-  .onWrite((event, context) => {
-    const roomData = event.after.data();
-    const participants = roomData.participants;
-
-    const registrationTokens = [];
-
-    const tokenOne = getDocument(participants[0]);
-    const tokenTwo = getDocument(participants[1]);
-
-    registrationTokens.push(tokenOne.fcm);
-    registrationTokens.push(tokenTwo.fcm);
-    // const fcmTokenOne = participantOne.fcm;
-    // const fcmTokenTwo = participantTwo.fcm;
-
-    console.log(registrationTokens);
-
-    const message = {
-      data: 'We have matched you with someone new!',
-      tokens: registrationTokens
-    }
-
-    admin.messaging().sendMulticast(message)
-      .then((response) => {
-      // eslint-disable-next-line promise/always-return
-      if (response.failureCount > 0 ) {
-        const failedTokens = [];
-        response.responses.forEach((resp, idx) => {
-          if (!resp.success) {
-            failedTokens.push(registrationTokens[idx]);
-          }
-        });
-        console.log('List of tokens that caused failures: ' + failedTokens);
-      }
-    })
-    .catch((error) => {
-      console.log('Error sending message:', error)
-    })
-  });
-
-async function getDocument(element) {
-  const docRef = db.collection('users').doc(`${element}`);
-
-  const doc = await docRef.get();
-  if (!doc.exists) {
-    console.log('No such document');
-    return '';
-  } else {
-    console.log('Document data:', doc.data().fcm);
-    return doc.data();
-  }
-}
-// exports.sendMessage = functions
-//   .runWith({
-//     timeoutSeconds: 540,
-//     memory: '2GB'
-//   })
-//   .https.onCall(async(data, context) => {
-//     if (!(context.auth && context.auth.token)) {
-//       throw new functions.https.HttpsError(
-//         'permission-denied',
-//         'Must be an administrative user to initiate messaging.'
-//       );
-//     }
-
-
-//     const fcmToken = data.fcm;
-//     const message = data.message;
-//     const options = {
-
-//     }
-
-//     console.log (
-//       `User ${context.auth.uid} has requested to send a message ${message}`
-//     )
-    
-//     await admin.messaging().sendToDevice(fcmToken, message, options) {
-
-//     }
-
-
-
-
-//   })
